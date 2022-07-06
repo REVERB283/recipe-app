f9b2e3d8616ad23a6dbcc6a225155bf97645368d
Harshit Anand <harshit.anand@rapipay.com>
2022-07-06 22:49:44 +0530

    eventemitter replaced with subjects

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,12 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable({
 	providedIn: "root",
 })
 export class ShoppingListService {
-	ingredientSelected = new EventEmitter<Ingredient[]>();
+	ingredientSelected = new Subject<Ingredient[]>();
 	private ingredients: Ingredient[] = [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 15)];
 
 	constructor() {}
@@ -16,11 +17,11 @@ export class ShoppingListService {
 
 	addIngredients(ingredient: Ingredient) {
 		this.ingredients.push(ingredient);
-		this.ingredientSelected.emit(this.ingredients.slice());
+		this.ingredientSelected.next(this.ingredients.slice());
 	}
 
 	addIngredientsList(ingredients: Ingredient[]) {
 		this.ingredients.push(...ingredients);
-		this.ingredientSelected.emit(this.ingredients.slice());
+		this.ingredientSelected.next(this.ingredients.slice());
 	}
 }
